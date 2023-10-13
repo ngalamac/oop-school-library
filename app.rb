f73ec9ebe2a6ea33fb9045240b423e59cbdde048
NGALA MAC <macngala4@gmail.com>
2023-10-13 11:49:59 +0100

    Add app.rb file

--- app.rb
+++ app.rb
@@ -1,161 +1,173 @@
 require_relative 'person'
 require_relative 'book'
 require_relative 'rental'
-require_relative 'student'
 require_relative 'teacher'
+require_relative 'student'
 require_relative 'classroom'
+require_relative 'specialization'
+require 'date'
 
 class App
-  attr_accessor :people, :books, :rentals
-
   def initialize
     @people = []
     @books = []
     @rentals = []
+    @classrooms = []
+    @specializations = []
   end
 
-  # Method to create a user input for creating a teacher or student
-  def create_user_input
+  def list_books
+    puts 'List of Books:'
+    @books.each { |book| puts " Title: #{book.title}, Author: #{book.author}" }
+  end
+
+  def list_people
+    puts 'List of People:'
+    @people.each do |person|
+      if person.is_a?(Teacher)
+        puts "[Teacher] Name:#{person.name} ID: #{person.id}  Age:#{person.age}"
+      elsif person.is_a?(Student)
+        puts "[Student] Name:#{person.name} ID:#{person.id} Age:#{person.age}"
+      else
+        puts "Unknown Type: Name: #{person.name} ID:#{person.id} Age:#{person.age}"
+      end
+    end
+  end
+
+  def create_person
     puts 'Do you want to create a teacher (1) or a student (2)? [Input the number]'
-    user_input = gets.chomp.to_i
-    if user_input == 1
+    type_option = gets.chomp.to_i
+
+    case type_option
+    when 1
       create_teacher
-    elsif user_input == 2
+    when 2
       create_student
     else
-      puts 'Invalid input'
+      puts 'Invalid option'
     end
   end
 
-  # Method to create a teacher
   def create_teacher
-    age, name, specialization = person_details
-    teacher = Teacher.new(specialization, age, name)
-    add_person(teacher)
-  end
-
-  # Method to create a student
-  def create_student
-    age, name, parent_permission_permission = person_details
-    student = Student.new(parent_permission,age, name)
-    add_person(student)
-  end
-
-  # Method to get common person details (age, name, specialization/parent permission)
-  def person_details
-    puts 'Age:'
+    puts 'Enter age:'
     age = gets.chomp.to_i
-    puts 'Name:'
-    name = gets.chomp.to_s
-    puts 'Specialization or parent permission (if student):'
-    specialization_or_permission = gets.chomp.to_s
-    [age, name, specialization_or_permission]
+
+    puts 'Enter name for the teacher:'
+    name = gets.chomp
+
+    puts 'Specialization:'
+    specialization_label = gets.chomp
+
+    specialization = find_or_create_specialization(specialization_label)
+
+    teacher = Teacher.new(name: name, age: age, specialization: specialization)
+    @people << teacher
+    puts 'Person created successfully!'
   end
 
-  # Method to add a person to the people array
-  def add_person(person)
-    @people.push(person)
-    puts "#{person.class.name} #{person.name} created successfully"
+  def find_or_create_specialization(label)
+    specialization = @specializations.find { |s| s.label == label }
+    if specialization.nil?
+      specialization = Specialization.new(label)
+      @specializations << specialization
+    end
+    specialization
   end
 
-  # Method to create a book
+  def create_student
+    puts 'Enter age:'
+    age = gets.chomp.to_i
+
+    puts 'Enter name for the student:'
+    name = gets.chomp
+
+    puts 'Has parent permission?(Y/N):'
+    parent_permission = gets.chomp.upcase == 'Y'
+
+    student = Student.new(name: name, age: age, parent_permission: parent_permission)
+    @people << student
+    puts 'Person created succesfully!'
+  end
+
+  def find_or_create_classroom(label)
+    classroom = @classrooms.find { |c| c.label == label }
+    unless classroom
+      classroom = Classroom.new(label)
+      @classrooms << classroom
+    end
+
+    classroom
+  end
+
   def create_book
-    puts 'Title of book:'
-    title = gets.chomp.to_s
-    puts 'Author of book:'
-    author = gets.chomp.to_s
+    puts 'Title:'
+    title = gets.chomp
+    puts 'Author:'
+    author = gets.chomp
     book = Book.new(title, author)
-    @books.push(book)
-    puts "Book #{book.title} created successfully"
+    @books << book
+    puts 'Book created successfully.'
   end
 
-  # Method to list books
-  def list_books
-    @books.each do |book|
-      puts "#{book.title} by #{book.author}"
-    end
-  end
-
-  # Method to list people (teachers and students)
-  def list_people
-    @people.each_with_index do |person, index|
-      puts "#{index + 1}. [#{person.class.name}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
-    end
-  end
-
-  # Method to create a rental
   def create_rental
-    book_index = select_book
-    return unless book_index
+    puts 'Select a person from the following list by number (not id):'
+    list_people_with_numbers
+    person_number = gets.chomp.to_i
 
-    person_index = select_person
-    return unless person_index
+    selected_person = @people[person_number - 1]
 
-    date = get_rental_date
+    if selected_person
+      puts 'Select a book from the following list by number'
+      list_books_with_numbers
+      book_number = gets.chomp.to_i
 
-    book = @books[book_index]
-    person = @people[person_index]
+      selected_book = @books[book_number - 1]
 
-    rental = Rental.new(date, book, person)
-    @rentals.push(rental)
-    puts 'Rental created successfully'
-  end
+      if selected_book
+        puts 'Date (YYYY-MM-DD):'
+        gets.chomp
 
-  # Method to select a book for rental
-  def select_book
-    puts 'Select a book from the following list by number'
-    book_list_with_index
-    book_index = gets.chomp.to_i
-    unless (0...@books.length).include?(book_index)
-      puts "Error adding rental, book with index #{book_index} doesn't exist"
-      return false
+        begin
+          date = Time.now
+          rental = Rental.new(date, selected_book, selected_person)
+          @rentals << rental
+          puts 'Rental created successfully'
+        rescue ArgumentError
+          puts 'Invalid date format. Please use YYYY-MM-DD.'
+        end
+      else
+        puts 'Invalid book selection'
+      end
+    else
+      puts 'Invalid person selection'
     end
-    book_index
   end
 
-  # Method to select a person for rental
-  def select_person
-    puts 'Select a person from the following list by number'
-    list_people_with_index
-    person_index = gets.chomp.to_i
-    unless (0...@people.length).include?(person_index)
-      puts "Error adding rental, person with index #{person_index} doesn't exist"
-      return false
-    end
-    person_index
-  end
-
-  # Method to get rental date
-  def get_rental_date
-    puts 'Date (YYYY-MM-DD):'
-    gets.chomp.to_s
-  end
-
-  # Method to list books with index
-  def book_list_with_index
+  def list_books_with_numbers
     @books.each_with_index do |book, index|
       puts "#{index + 1}. #{book.title} by #{book.author}"
     end
   end
 
-  # Method to list people with index
-  def list_people_with_index
+  def list_people_with_numbers
     @people.each_with_index do |person, index|
-      puts "#{index + 1}. [#{person.class.name}] Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
+      puts "#{index + 1}. #{person.name} (ID: #{person.id})"
     end
   end
 
-  # Method to list rentals for a given person ID
-  def list_rentals
-    puts 'Enter person ID'
+  def list_rentals_for_person
+    puts 'ID of person:'
     person_id = gets.chomp.to_i
-    rentals = @rentals.select { |rental| rental.person.id == person_id }
-    puts 'Rentals:'
-    rentals.each do |rental|
-      puts "Date: #{rental.date}, Book: #{rental.book.title}"
-    end
-    return unless rentals.empty?
 
-    puts "No rentals found for this person with ID #{person_id}"
+    person = @people.find { |p| p.id == person_id }
+
+    if person
+      puts 'Rentals:'
+      person.rentals.each do |rental|
+        puts " Date: #{rental.date.strftime('%y-%m-%d')}, Book: #{rental.book.title} by #{rental.book.author} "
+      end
+    else
+      puts 'Person not found.'
+    end
   end
 end
